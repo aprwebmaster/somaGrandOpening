9f51f3f03c2ac4c18a8f6a9cecfb7c2082006d3f
aprwebmaster <cbradford@apr.com>
2016-11-30 16:18:06 -0800

    added basic bootstrap styling to registration pages, added view for new sessions, edited application.html.erb tp allow for additional variables for users to be added to the database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-end
+ class ApplicationController < ActionController::Base
+   protect_from_forgery with: :exception
+   before_action :configure_permitted_parameters, if: :devise_controller?
+ 
+   protected
+ 
+   def configure_permitted_parameters
+     devise_parameter_sanitizer.for(:sign_up) { |su| su.permit(:name, :phone_number, :address, :number_of_guests) }
+   end
+
+
+ end
